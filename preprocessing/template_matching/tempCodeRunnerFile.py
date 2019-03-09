b130990ffa6a563057e46885c76cee17e7994def
Mathias Nielsen <mathiias@live.dk>
2019-03-10 00:32:16 +0100

    Update

---
+++ preprocessing/template_matching/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+    # match_space = cv2.normalize(result,
+    #                             None,
+    #                             0,
+    #                             255,
+    #                             norm_type=cv2.NORM_MINMAX)
