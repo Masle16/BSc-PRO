60575b925de3467a5aca6eaf80b43a588e99c2d0
Mikkel <mikkellars@hotmail.com>
2019-03-15 13:53:21 +0100

    Back-projection

--- preprocessing/color_picker.py
+++ preprocessing/color_picker.py
@@ -1,4 +1,8 @@
-""" Module for color picker """
+#!/home/mikkel/anaconda2/envs/NN1.8/bin/python
+
+"""
+Module for color picker
+"""
 
 import cv2
 import numpy as np
@@ -11,7 +15,7 @@ def main():
     """ Main function """
 
     # Create a black image, a window
-    img_path = '/mnt/sdb1/Robtek/6semester/Bachelorproject/BSc-PRO/images_1280x720/baggrund/bevægelse/WIN_20190131_10_31_36_Pro.jpg'
+    img_path = '/home/mikkel/Documents/github/BSc-PRO/images_1280x720/baggrund/bevægelse/WIN_20190131_10_31_36_Pro.jpg'
     img = cv2.imread(img_path, cv2.IMREAD_COLOR)
     cv2.namedWindow('Trackbars')
 
