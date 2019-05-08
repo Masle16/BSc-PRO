a9d81329b735010c9b1898df49c10c97410a4d3f
Mikkel <mikkellars@hotmail.com>
2019-05-08 13:52:42 +0200

    updated cnn model

--- Classification/nets/cnn_net/cnn_net.py
+++ Classification/nets/cnn_net/cnn_net.py
@@ -23,3 +23,47 @@ def cnn_net():
                   optimizer=Adam(lr=0.001, epsilon=10e-8),
                   metrics=['accuracy'])
     return model
+
+def cnn_net_small_dataset():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), input_shape=(224, 224, 3)))    # 32
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, (3, 3)))                               # 64
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Flatten())  #  3D activation map -> 1D vector
+    model.add(Dense(32))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(3))
+    model.add(Activation('softmax'))
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=Adam(lr=0.001, epsilon=10e-8),
+                  metrics=['accuracy'])
+    return model
+
+def cnn_net_large_dataset():
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), input_shape=(224, 224, 3)))    # 32
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, (3, 3)))                               # 64
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Flatten())  #  3D activation map -> 1D vector
+    model.add(Dense(32))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(8))
+    model.add(Activation('softmax'))
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=Adam(lr=0.001, epsilon=10e-8),
+                  metrics=['accuracy'])
+    return model
