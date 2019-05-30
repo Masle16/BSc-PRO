f49061500a6181ff37f91eb52dbc05b2f21b9296
Mikkel Larsen <31695685+mikkellars@users.noreply.github.com>
2019-05-30 21:04:30 +0200

    1

--- Classification/Prepocessing_plots.m
+++ Classification/Prepocessing_plots.m
@@ -13,9 +13,10 @@ z_mean_keras = textread('z_mean_keras.txt');
 
 %% Plot for original data versus our mean image subtraction
 figure(1)
-scatter3(x,y,z)
-hold on
 scatter3(x_mean, y_mean, z_mean)
+%scatter3(x,y,z)
+hold on
+%scatter3(x_mean, y_mean, z_mean)
 legend('Images pixels', 'Image pixels subtracted mean image')
 title('All downscaled images(16x16) pixel values versus images subtracted with mean image')
 xlabel('Red')
@@ -24,9 +25,10 @@ zlabel('Blue')
 hold off
 %% Plot for original data versus keras mean RGB subtraction
 figure(2)
-scatter3(x,y,z)
-hold on
 scatter3(x_mean_keras, y_mean_keras, z_mean_keras)
+%scatter3(x,y,z)
+hold on
+
 legend('Images pixels', 'Images pixels subtracted mean rgb ')
 title('All downscaled images(16x16) pixel values versus images subtracted with mean rgb values')
 xlabel('Red')
@@ -35,3 +37,5 @@ zlabel('Blue')
 hold off
 
 %% Plot whitening versus not whitening
+figure(3)
+scatter3(x,y,z)
--- Classification/hyperparameter_plot.m
+++ Classification/hyperparameter_plot.m
@@ -2,9 +2,10 @@
 hidden_size = [32, 64, 128, 256, 512]
 validation_acc = [90.4545, 90.6818, 89.0909, 88.1818, 88.8636]
 
-plot(hidden_size, validation_acc)
-title('Validation accuracy versus hidden size with Learning rate = 1e-4')
-xlabel('Hidden size')
-ylabel('Accuracy in percent')
+plot(hidden_size, validation_acc,'-s','MarkerSize',12, 'MarkerFaceColor','blue', 'LineWidth', 2)
+xlabel('Hidden size', 'FontSize', 24)
+ylabel('Accuracy [%]', 'FontSize', 24)
+xlim([0 520])
 xticks([32, 64, 128, 256, 512])
-legend('Hidden size')
+set(gca,'FontSize',24)
+legend('Hidden size', 'FontSize', 24)
